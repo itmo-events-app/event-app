7474be028ab493c7891ed10714a9909f3210a57c
Sofya Inglikova <sofiainglikova@gmail.com>
2024-03-28 21:23:52 +0300

    fix: method signatures and request methods in task controller fixed; feat: validation added

--- src/main/java/org/itmo/eventapp/main/model/dto/TaskDto.java
+++ src/main/java/org/itmo/eventapp/main/model/dto/TaskDto.java
@@ -1,17 +1,27 @@
 package org.itmo.eventapp.main.model.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.itmo.eventapp.main.model.entity.TaskStatus;
 
 import java.time.LocalDateTime;
 
 public record TaskDto(Integer id,
-               Integer eventId, // validate not null
-               UserShortDataDto assignee,
-               UserShortDataDto assigner, // validate not null
-               String title, // validate not blank
-               String description,
-               TaskStatus taskStatus,
-               PlaceShortDataDto place,
-               LocalDateTime deadline,
-               LocalDateTime notificationDeadline) {
+                      @Min(1)
+                      Integer eventId, // validate not null and > 0
+                      UserShortDataDto assignee,
+                      @NotNull
+                      UserShortDataDto assigner, // validate not null
+                      @NotBlank
+                      String title, // validate not blank
+                      @NotNull
+                      String description,
+                      @NotNull
+                      TaskStatus taskStatus,
+                      PlaceShortDataDto place,
+                      @NotNull
+                      LocalDateTime deadline,
+                      @NotNull
+                      LocalDateTime notificationDeadline) {
 }
