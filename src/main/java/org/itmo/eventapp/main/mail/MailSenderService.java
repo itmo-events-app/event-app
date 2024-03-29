1a628566dee608d0f9b6ab8195d52cfcd2fa2fe7
Alexander Kuznecov <darklol545@gmail.com>
2024-03-29 16:45:28 +0300

    mail sender methods implemented, with html email templates

--- src/main/java/org/itmo/eventapp/main/mail/MailSenderService.java
+++ src/main/java/org/itmo/eventapp/main/mail/MailSenderService.java
@@ -1,23 +1,28 @@
 package org.itmo.eventapp.main.mail;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.util.FileCopyUtils;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Сервис для отправки писем по электронной почте
  */
 @Component
 public class MailSenderService {
-    /*
-    TODO: сделать HTML темплейты для писем
-    TODO: сделать все нужные методы
-     */
+    //todo добавить логирование
 
     @Autowired
     private JavaMailSender mailSender;
@@ -25,27 +30,105 @@ public class MailSenderService {
     private String senderName;
 
     /**
-     * Метод отправляет писмо пользователю с уведомлением о новой задаче
+     * Отправляет писмо на указанный email с уведомлением о новой задаче
+     * Использует шаблон письма о создании задачи
      * @param userEmail
      * @param userName
      * @param eventName
      * @param taskName
+     * @param taskLink
      */
     @Async
-    public void sendCreatedTaskMessage(String userEmail, String userName, String eventName, String taskName) {
+    public void sendIncomingTaskMessage(String userEmail, String userName, String eventName, String taskName, String taskLink) {
         try {
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setTo(userEmail);
-            //обязательно нужно указать sender для mail.ru
+            MimeMessage message = mailSender.createMimeMessage();
             message.setFrom(senderName);
+            message.setRecipients(MimeMessage.RecipientType.TO, userEmail);
             message.setSubject("Новая задача!");
-            message.setText("Дорогой пользователь " + userName + "!\n"
-                    + "Вам на выполнение поступила новая задача - \"" + taskName + "\" "
-                    + "в мероприятии \"" + eventName + "\", не забудте выполнить её в срок!");
+            String messageContent = readTemplate("classpath:email-templates/incoming-task-template")
+                    .replace("${userName}", userName)
+                    .replace("${eventName}", eventName)
+                    .replace("${taskName}", taskName)
+                    .replace("${taskLink}", taskLink);
+            message.setContent(messageContent, "text/html; charset=utf-8");
             mailSender.send(message);
-        } catch (MailException e) {
+        } catch (MailException | MessagingException e) {
+            //логирование
+            e.printStackTrace();
+        } catch (IOException e) {
             //логирование
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Отправляет писмо на указанный email с уведомлением о просроченной задаче
+     * Использует шаблон письма о просроченной задачи
+     * @param userEmail
+     * @param userName
+     * @param eventName
+     * @param taskName
+     * @param taskLink
+     */
+    @Async
+    public void sendOverdueTaskMessage(String userEmail, String userName, String eventName, String taskName, String taskLink) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            message.setFrom(senderName);
+            message.setRecipients(MimeMessage.RecipientType.TO, userEmail);
+            message.setSubject("Просроченная задача!");
+            String messageContent = readTemplate("classpath:email-templates/overdue-task-template")
+                    .replace("${userName}", userName)
+                    .replace("${eventName}", eventName)
+                    .replace("${taskName}", taskName)
+                    .replace("${taskLink}", taskLink);
+            message.setContent(messageContent, "text/html; charset=utf-8");
+            mailSender.send(message);
+        } catch (MailException | MessagingException e) {
+            //логирование
+            e.printStackTrace();
+        } catch (IOException e) {
+            //логирование
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Отправляет писмо на указанный email с напоминанием в выполнении задачи
+     * Использует шаблон письма с напоминанием о задаче
+     * @param userEmail
+     * @param userName
+     * @param eventName
+     * @param taskName
+     * @param taskLink
+     */
+    @Async
+    public void sendReminderTaskMessage(String userEmail, String userName, String eventName, String taskName, String taskLink) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            message.setFrom(senderName);
+            message.setRecipients(MimeMessage.RecipientType.TO, userEmail);
+            message.setSubject("Не забудьте выполнить задачу!");
+            String messageContent = readTemplate("classpath:email-templates/reminder-task-template")
+                    .replace("${userName}", userName)
+                    .replace("${eventName}", eventName)
+                    .replace("${taskName}", taskName)
+                    .replace("${taskLink}", taskLink);
+            message.setContent(messageContent, "text/html; charset=utf-8");
+            mailSender.send(message);
+        } catch (MailException | MessagingException e) {
+            //логирование
+            e.printStackTrace();
+        } catch (IOException e) {
+            //логирование
+            e.printStackTrace();
+        }
+    }
+
+    //Читает файл шаблона и преобразует в String
+    private String readTemplate(String templatePath) throws IOException {
+        Resource resource = new ClassPathResource(templatePath);
+        Reader reader = new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8);
+        return FileCopyUtils.copyToString(reader);
     }
 }
---
+++ src/test/java/org/itmo/eventApp/main/controller/EmailSendingTest.java
@@ -0,0 +1,13 @@
+package org.itmo.eventApp.main.controller;
+
+import org.itmo.eventapp.main.mail.MailSenderService;
+import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class EmailSendingTest extends AbstractTestContainers{
+    @Autowired
+    private MailSenderService mailSenderService;
+
+    private GreenMailExtension
+
+}
