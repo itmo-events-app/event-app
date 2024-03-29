164d8276cb9e0eb60c81fbab79e38e6c6e7c56a9
Roman Glinskikh <glinskikh.roman@yandex.ru>
2024-03-29 15:53:02 +0300

    role-privilege join fix

--- src/main/java/org/itmo/eventapp/main/model/entity/Role.java
+++ src/main/java/org/itmo/eventapp/main/model/entity/Role.java
@@ -25,9 +25,9 @@ public class Role {
     private RoleType type;
 
     @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    @JoinTable(name="privileges",
+    @JoinTable(name="privilege",
             joinColumns=  @JoinColumn(name="role_id", referencedColumnName="id"),
-            inverseJoinColumns= @JoinColumn(name="privileges_id", referencedColumnName="id") )
+            inverseJoinColumns= @JoinColumn(name="privilege_id", referencedColumnName="id") )
     private Set<Privilege> privileges = new HashSet<Privilege>();
 
     public Role() {
