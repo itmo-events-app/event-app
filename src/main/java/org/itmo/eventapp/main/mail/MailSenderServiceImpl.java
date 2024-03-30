634837df286eba6c971aa33a87f7e14d84372be6
Alexander Kuznecov <darklol545@gmail.com>
2024-03-30 11:07:41 +0300

    very small refactoring

--- src/main/java/org/itmo/eventapp/main/mail/MailSenderServiceImpl.java
+++ src/main/java/org/itmo/eventapp/main/mail/MailSenderServiceImpl.java
@@ -2,14 +2,14 @@ package org.itmo.eventapp.main.mail;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.util.FileCopyUtils;
 
 import java.io.IOException;
@@ -20,11 +20,11 @@ import java.nio.charset.StandardCharsets;
 /**
  * Реализация сервиса для отправки писем по электронной почте
  */
-@Component
+@RequiredArgsConstructor
+@Service
 public class MailSenderServiceImpl implements MailSenderService{
     //todo добавить логирование
 
-    @Autowired
     private JavaMailSender mailSender;
     @Value("${spring.mail.username}")
     private String senderName;
@@ -37,7 +37,7 @@ public class MailSenderServiceImpl implements MailSenderService{
             message.setFrom(senderName);
             message.setRecipients(MimeMessage.RecipientType.TO, userEmail);
             message.setSubject("Новая задача!");
-            String messageContent = readTemplate("email-templates/incoming-task-template.html")
+            String messageContent = readTemplate("notification/email-templates/incoming-task-template.html")
                     .replace("${userName}", userName)
                     .replace("${eventName}", eventName)
                     .replace("${taskName}", taskName)
@@ -61,7 +61,7 @@ public class MailSenderServiceImpl implements MailSenderService{
             message.setFrom(senderName);
             message.setRecipients(MimeMessage.RecipientType.TO, userEmail);
             message.setSubject("Просроченная задача!");
-            String messageContent = readTemplate("email-templates/overdue-task-template.html")
+            String messageContent = readTemplate("notification/email-templates/overdue-task-template.html")
                     .replace("${userName}", userName)
                     .replace("${eventName}", eventName)
                     .replace("${taskName}", taskName)
@@ -85,7 +85,7 @@ public class MailSenderServiceImpl implements MailSenderService{
             message.setFrom(senderName);
             message.setRecipients(MimeMessage.RecipientType.TO, userEmail);
             message.setSubject("Не забудьте выполнить задачу!");
-            String messageContent = readTemplate("email-templates/reminder-task-template.html")
+            String messageContent = readTemplate("notification/email-templates/reminder-task-template.html")
                     .replace("${userName}", userName)
                     .replace("${eventName}", eventName)
                     .replace("${taskName}", taskName)
