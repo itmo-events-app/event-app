3879837b606d9414a12ec2b2879d646cf09f83bc
Alexander Kuznecov <darklol545@gmail.com>
2024-03-30 13:18:18 +0300

    lib version deleted

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-mail:3.2.4")
+    implementation("org.springframework.boot:spring-boot-starter-mail")
     implementation("org.flywaydb:flyway-core:9.22.3")
     implementation("org.springframework.boot:spring-boot-starter-validation")
 
