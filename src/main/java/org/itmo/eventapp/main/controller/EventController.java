b67c0f9f79a2cd9c05d2123389fd3275a30b2215
thermvs <mariagewitter@gmail.com>
2024-04-01 11:21:32 +0300

    fix: remove usage of generic wildcard type

--- src/main/java/org/itmo/eventapp/main/controller/EventController.java
+++ src/main/java/org/itmo/eventapp/main/controller/EventController.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.itmo.eventapp.main.model.dto.request.EventRequest;
+import org.itmo.eventapp.main.model.dto.response.EventResponse;
 import org.itmo.eventapp.main.service.EventService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -16,6 +17,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 
 @RequiredArgsConstructor
@@ -31,19 +34,19 @@ public class EventController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<?> updateEvent(@Min(1) @PathVariable("id") Integer id,
-                                         @RequestBody @Valid EventRequest eventRequest) {
+    public ResponseEntity<EventResponse> updateEvent(@Min(1) @PathVariable("id") Integer id,
+                                                     @RequestBody @Valid EventRequest eventRequest) {
         return ResponseEntity.ok().body(eventService.updateEvent(id, eventRequest));
     }
 
     @GetMapping("/")
-    public ResponseEntity<?> getAllEvents(@Min(0) @RequestParam(value = "page", defaultValue = "0") int page,
-                                          @Min(0) @Max(50) @RequestParam(value = "size", defaultValue = "15") int size) {
+    public ResponseEntity<List<EventResponse>> getAllEvents(@Min(0) @RequestParam(value = "page", defaultValue = "0") int page,
+                                                            @Min(0) @Max(50) @RequestParam(value = "size", defaultValue = "15") int size) {
         return ResponseEntity.ok().body(eventService.getAllEvents(page, size));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getEventById(@Min(1) @PathVariable("id") Integer id) {
+    public ResponseEntity<EventResponse> getEventById(@Min(1) @PathVariable("id") Integer id) {
         return ResponseEntity.ok().body(eventService.getEventById(id));
     }
 }
