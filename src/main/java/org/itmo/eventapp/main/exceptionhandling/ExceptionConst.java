1091b877971727e3253f8e83e1005a45cbad9da5
thermvs <mariagewitter@gmail.com>
2024-04-01 23:41:55 +0300

    feat: add a private constructor

--- src/main/java/org/itmo/eventapp/main/exceptionhandling/ExceptionConst.java
+++ src/main/java/org/itmo/eventapp/main/exceptionhandling/ExceptionConst.java
@@ -4,4 +4,7 @@ public class ExceptionConst {
     public static final String EVENT_NOT_FOUND_MESSAGE = "Event not found";
     public static final String EVENT_PARENT_NOT_FOUND_MESSAGE = "Event's parent not found";
     public static final String PLACE_NOT_FOUND_MESSAGE = "Place not found";
+
+    private ExceptionConst() {
+    }
 }
