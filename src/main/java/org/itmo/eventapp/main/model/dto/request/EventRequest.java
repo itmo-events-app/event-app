0e8cd34d84f79a5d2eee0b18950763918e7c95b4
Roman Glinskikh <glinskikh.roman@yandex.ru>
2024-04-02 11:53:01 +0300

    notnull

--- src/main/java/org/itmo/eventapp/main/model/dto/request/EventRequest.java
+++ src/main/java/org/itmo/eventapp/main/model/dto/request/EventRequest.java
@@ -32,7 +32,6 @@ public record EventRequest(
         LocalDateTime registrationStart,
         @NotNull(message = "Поле registrationEnd не может быть null!")
         LocalDateTime registrationEnd,
-        @NotNull(message = "Поле parent не может быть null!")
         Integer parent,
         @NotNull(message = "Поле participantLimit не может быть null!")
         @Min(value = 1, message = "Поле participantLimit не может быть меньше 1!")
