712d8f639daddbe6dd316daa5bda728090061a19
Alexander Kuznecov <darklol545@gmail.com>
2024-04-05 14:01:10 +0300

    Task repo queries added

--- src/main/java/org/itmo/eventapp/main/repository/TaskRepository.java
+++ src/main/java/org/itmo/eventapp/main/repository/TaskRepository.java
@@ -4,6 +4,11 @@ import org.itmo.eventapp.main.model.entity.Task;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface TaskRepository extends ListCrudRepository<Task, Integer> {
+    List<Task> findAllByNotificationDeadlineAfter(LocalDateTime dateTime);
+    List<Task> findAllByDeadlineAfter(LocalDateTime dateTime);
 }
