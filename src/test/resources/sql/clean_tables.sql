6fe9f79ca4b1d0bab5cfebff8808c5583f139016
Kim Daniel <kimdaniiell@gmail.com>
2024-04-05 14:44:04 +0300

    add role cleaning

--- src/test/resources/sql/clean_tables.sql
+++ src/test/resources/sql/clean_tables.sql
@@ -9,6 +9,11 @@ truncate table user_t cascade;
 truncate table user_notification_info cascade;
 truncate table registration_request cascade;
 
+truncate table role cascade; -- todo
+truncate table privilege cascade; -- todo
+truncate table role_privilege cascade; -- todo
+
+
 -- reset id sequences
 select setval('task_id_seq', 1, false);
 select setval('event_role_id_seq', 1, false);
@@ -21,3 +26,10 @@ select setval('user_t_id_seq', 1, false);
 -- select setval('user_notification_info_id_seq', 1, false); на этом варианте валилось
 alter sequence user_notification_info_id_seq restart with 1; -- Для остальных мб тоже лучше так
 select setval('registration_request_id_seq', 1, false);
+
+-- select setval('role_id_seq', 1, false); -- todo
+alter sequence role_id_seq restart with 1;
+-- select setval('privilege_id_seq', 1, false); -- todo
+alter sequence privilege_id_seq restart with 1;
+
+alter sequence role_privilege_id_seq restart with 1;
