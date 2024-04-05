bf3fcf51d252b38405288067672ed9ee51b966f0
Alexander Kuznecov <darklol545@gmail.com>
2024-04-05 12:18:45 +0300

    working tests for NotificationController

--- src/test/java/org/itmo/eventApp/main/controller/NotificationControllerTest.java
+++ src/test/java/org/itmo/eventApp/main/controller/NotificationControllerTest.java
@@ -1,11 +1,13 @@
 package org.itmo.eventApp.main.controller;
 
+import org.itmo.eventapp.main.model.entity.User;
+import org.itmo.eventapp.main.model.entity.UserLoginInfo;
 import org.junit.jupiter.api.Test;
-import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.security.test.context.support.WithMockUser;
 
 import static org.hamcrest.Matchers.hasSize;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
@@ -20,51 +22,61 @@ public class NotificationControllerTest extends AbstractTestContainers{
         executeSqlScript("/sql/insert_notifications.sql");
     }
 
+    private UserLoginInfo getUserLoginInfo(Integer userId){
+        UserLoginInfo userDetails = new UserLoginInfo();
+        User dummyUser = new User();
+        dummyUser.setId(1);
+        userDetails.setUser(dummyUser);
+        return userDetails;
+    }
+
+
     @Test
-//    @WithUserDetails("test_mail@test_mail.com")
+    @WithMockUser(username = "test_mail@itmo.ru")
     void getNotificationsTest() throws Exception {
-//        databaseFilling();
-//        mockMvc.perform(get("/api/notifications")
-//                        .param("page", "0")
-//                        .param("size", "10"))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$").isNotEmpty())
-//                .andExpect(jsonPath("$").isArray())
-//                .andExpect(jsonPath("$", hasSize(2)))
-//                .andExpect(jsonPath("$[0].id").value("1"))
-//                .andExpect(jsonPath("$[1].id").value("2"));
-        assertTrue(true);
+        databaseFilling();
+        mockMvc.perform(get("/api/notifications")
+                        .param("page", "0")
+                        .param("size", "10")
+                        .with(user(getUserLoginInfo(1))))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isNotEmpty())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].id").value("1"))
+                .andExpect(jsonPath("$[1].id").value("2"));
     }
 
     @Test
-//    @WithUserDetails("test_mail@test_mail.com")
+    @WithMockUser(username = "test_mail@itmo.ru")
     void allSeenNotificationsTest() throws Exception {
-//        databaseFilling();
-//        mockMvc.perform(put("/api/notifications")
-//                        .param("page", "0")
-//                        .param("size", "10"))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$").isNotEmpty())
-//                .andExpect(jsonPath("$").isArray())
-//                .andExpect(jsonPath("$", hasSize(2)))
-//                .andExpect(jsonPath("$[0].id").value("1"))
-//                .andExpect(jsonPath("$[1].id").value("2"))
-//                .andExpect(jsonPath("$[0].seen").value("true"))
-//                .andExpect(jsonPath("$[1].seen").value("true"));
-        assertTrue(true);
+        databaseFilling();
+        mockMvc.perform(put("/api/notifications")
+                        .param("page", "0")
+                        .param("size", "10")
+                        .with(user(getUserLoginInfo(1))))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isNotEmpty())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].id").value("1"))
+                .andExpect(jsonPath("$[1].id").value("2"))
+                .andExpect(jsonPath("$[0].seen").value("true"))
+                .andExpect(jsonPath("$[1].seen").value("true"));
     }
 
     @Test
-//    @WithUserDetails("test_mail@test_mail.com")
+    @WithMockUser(username = "test_mail@itmo.ru")
     void oneSeenNotificationsTest() throws Exception {
-//        databaseFilling();
-//        mockMvc.perform(put("/api/notifications/1")
-//                        .param("page", "0")
-//                        .param("size", "10"))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$").isNotEmpty())
-//                .andExpect(jsonPath("$.id").value("1"))
-//                .andExpect(jsonPath("$.seen").value("true"));
+        databaseFilling();
+        mockMvc.perform(put("/api/notifications/1")
+                        .param("page", "0")
+                        .param("size", "10")
+                        .with(user(getUserLoginInfo(1))))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isNotEmpty())
+                .andExpect(jsonPath("$.id").value("1"))
+                .andExpect(jsonPath("$.seen").value("true"));
         assertTrue(true);
     }
 
