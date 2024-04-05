f51c347134b446272f394c495b2410470e752fb7
Alexander Kuznecov <darklol545@gmail.com>
2024-04-05 12:17:02 +0300

    mail change in user_insert scripts

--- src/test/resources/sql/insert_user.sql
+++ src/test/resources/sql/insert_user.sql
@@ -1,7 +1,7 @@
 -- note - password is: password
 
 insert into registration_request (email, password_hash, name, surname, status, sent_time)
-values ('test_mail@test_mail.com', '$2a$10$DP4EvNpQKbgSwi68DGyZcOeK.t12oCxd1D0ZC09LVZb69wqYE8u0K', 'test', 'user', 'APPROVED', '2024-03-30T21:32:23.536819');
+values ('test_mail@itmo.ru', '$2a$10$DP4EvNpQKbgSwi68DGyZcOeK.t12oCxd1D0ZC09LVZb69wqYE8u0K', 'test', 'user', 'APPROVED', '2024-03-30T21:32:23.536819');
 
 insert into user_notification_info (devices, enable_push_notifications, enable_email_notifications)
 values ('{web, android}', false, false);
@@ -9,4 +9,4 @@ values ('{web, android}', false, false);
 insert into user_t (role_id, notification_info_id, name, surname) values (1, 1, 'test', 'user');
 
 insert into user_login_info (user_id, email, email_status, password_hash, reset_token, last_login_date, registration_id)
-values (1, 'test_mail@test_mail.com', 'APPROVED', '$2a$10$DP4EvNpQKbgSwi68DGyZcOeK.t12oCxd1D0ZC09LVZb69wqYE8u0K', 'reset token', '2024-03-30T21:32:23.536819', 1);
+values (1, 'test_mail@itmo.ru', 'APPROVED', '$2a$10$DP4EvNpQKbgSwi68DGyZcOeK.t12oCxd1D0ZC09LVZb69wqYE8u0K', 'reset token', '2024-03-30T21:32:23.536819', 1);
--- src/test/resources/sql/insert_user_2.sql
+++ src/test/resources/sql/insert_user_2.sql
@@ -1,7 +1,7 @@
 -- note - password is: password
 
 insert into registration_request (email, password_hash, name, surname, status, sent_time)
-values ('test_mail2@test_mail.com', '$2a$10$DP4EvNpQKbgSwi68DGyZcOeK.t12oCxd1D0ZC09LVZb69wqYE8u0K', 'test2', 'user2', 'APPROVED', '2024-03-30T21:32:23.536819');
+values ('test_mail2@itmo.ru', '$2a$10$DP4EvNpQKbgSwi68DGyZcOeK.t12oCxd1D0ZC09LVZb69wqYE8u0K', 'test2', 'user2', 'APPROVED', '2024-03-30T21:32:23.536819');
 
 insert into user_notification_info (devices, enable_push_notifications, enable_email_notifications)
 values ('{web, android}', false, false);
@@ -9,4 +9,4 @@ values ('{web, android}', false, false);
 insert into user_t (role_id, notification_info_id, name, surname) values (1, 2, 'test2', 'user2');
 
 insert into user_login_info (user_id, email, email_status, password_hash, reset_token, last_login_date, registration_id)
-values (2, 'test_mail2@test_mail.com', 'APPROVED', '$2a$10$DP4EvNpQKbgSwi68DGyZcOeK.t12oCxd1D0ZC09LVZb69wqYE8u0K', 'reset token', '2024-03-30T21:32:23.536819', 2);
+values (2, 'test_mail2@itmo.ru', 'APPROVED', '$2a$10$DP4EvNpQKbgSwi68DGyZcOeK.t12oCxd1D0ZC09LVZb69wqYE8u0K', 'reset token', '2024-03-30T21:32:23.536819', 2);
