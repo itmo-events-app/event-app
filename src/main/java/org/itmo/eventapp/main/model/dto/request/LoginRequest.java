99f9f1a31d036aaa8f04ba077270d009f5f2c0c1
a.verz <alxverzz@gmail.com>
2024-04-07 21:06:19 +0300

    fix: remove login and password validation from LoginRequest

--- src/main/java/org/itmo/eventapp/main/model/dto/request/LoginRequest.java
+++ src/main/java/org/itmo/eventapp/main/model/dto/request/LoginRequest.java
@@ -1,17 +1,12 @@
 package org.itmo.eventapp.main.model.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import org.itmo.eventapp.main.model.validation.annotation.StrongPassword;
 
 
 public record LoginRequest(
     @NotBlank(message = "Поле обязательно для заполнения")
-    @Pattern(regexp = "^\\w[\\w\\-.]*@(niu|idu.)?itmo\\.ru$",
-            message = "Должен использоваться email с доменом Университета ИТМО (@itmo.ru, @idu.itmo.ru или @niuitmo.ru)")
     String login,
 
     @NotBlank(message = "Поле обязательно для заполнения")
-    @StrongPassword
     String password
 ) {}
