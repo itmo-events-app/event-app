20bc08fa28f716fc197f1e4aa33a79107c116bef
HackMemory <dragon.warri0r@yandex.com>
2024-04-09 02:20:13 +0300

    fix sonar 2

--- src/main/java/org/itmo/eventapp/main/model/dto/response/ProfileResponse.java
+++ src/main/java/org/itmo/eventapp/main/model/dto/response/ProfileResponse.java
@@ -1,7 +1,9 @@
 package org.itmo.eventapp.main.model.dto.response;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public record ProfileResponse(
         Integer userId,
@@ -14,4 +16,37 @@ public record ProfileResponse(
         String[] devices
 
 ) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProfileResponse that = (ProfileResponse) o;
+        return enablePushNotifications == that.enablePushNotifications &&
+                enableEmailNotifications == that.enableEmailNotifications &&
+                Objects.equals(userId, that.userId) &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(surname, that.surname) &&
+                Objects.equals(userInfo, that.userInfo) &&
+                Objects.equals(lastLoginDate, that.lastLoginDate) &&
+                Arrays.equals(devices, that.devices);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, name, surname, userInfo, lastLoginDate, enablePushNotifications, enableEmailNotifications, Arrays.hashCode(devices));
+    }
+
+    @Override
+    public String toString() {
+        return "ProfileResponse{" +
+                "userId=" + userId +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", userInfo=" + userInfo +
+                ", lastLoginDate=" + lastLoginDate +
+                ", enablePushNotifications=" + enablePushNotifications +
+                ", enableEmailNotifications=" + enableEmailNotifications +
+                ", devices=" + Arrays.toString(devices) +
+                '}';
+    }
 }
