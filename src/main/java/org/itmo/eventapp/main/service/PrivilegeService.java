d89e548c60ed8b3d5934ab8a6879b975f9c7055c
Kim Daniel <kimdaniiell@gmail.com>
2024-04-09 05:43:07 +0300

    fix: use ExceptionConst

--- src/main/java/org/itmo/eventapp/main/service/PrivilegeService.java
+++ src/main/java/org/itmo/eventapp/main/service/PrivilegeService.java
@@ -1,17 +1,15 @@
 package org.itmo.eventapp.main.service;
 
 import lombok.RequiredArgsConstructor;
-import org.itmo.eventapp.main.model.dto.response.PrivilegeResponse;
+import org.itmo.eventapp.main.exceptionhandling.ExceptionConst;
 import org.itmo.eventapp.main.model.entity.Privilege;
 import org.itmo.eventapp.main.model.entity.enums.PrivilegeName;
 import org.itmo.eventapp.main.model.entity.enums.PrivilegeType;
-import org.itmo.eventapp.main.model.mapper.PrivilegeMapper;
 import org.itmo.eventapp.main.repository.PrivilegeRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Collection;
 import java.util.List;
 
 @Service
@@ -21,15 +19,22 @@ public class PrivilegeService {
 
     public Privilege findById(Integer id) {
         return privilegeRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Привилегия с id %d не существует", id)));
+                .orElseThrow(() -> new ResponseStatusException(
+                                HttpStatus.NOT_FOUND,
+                                ExceptionConst.PRIVILEGE_ID_NOT_FOUND_MESSAGE.formatted(id)));
     }
 
     public List<Privilege> getPrivilegeByType(PrivilegeType type) {
-        var privileges = privilegeRepository.findAllByType(type);
+        List<Privilege> privileges = privilegeRepository.findAllByType(type);
+
         if (type.equals(PrivilegeType.SYSTEM)) {
-            var assignOrganizerPrivilege = privilegeRepository.findByName(PrivilegeName.ASSIGN_ORGANIZER_ROLE);
-            privileges.add(assignOrganizerPrivilege);
+            Privilege assignOrganizerPrivilege =
+                    privilegeRepository.findByName(
+                            PrivilegeName.ASSIGN_ORGANIZER_ROLE);
+            if (null != assignOrganizerPrivilege)
+                privileges.add(assignOrganizerPrivilege);
         }
+
         return privileges;
     }
 
