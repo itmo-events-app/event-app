bdd2afc9ab644ea2df32aaedd6486f776113f019
HackMemory <dragon.warri0r@yandex.com>
2024-04-09 14:50:23 +0300

    fix: unused import

--- src/main/java/org/itmo/eventapp/main/model/validation/annotation/ValidLogin.java
+++ src/main/java/org/itmo/eventapp/main/model/validation/annotation/ValidLogin.java
@@ -3,10 +3,12 @@ package org.itmo.eventapp.main.model.validation.annotation;
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 import org.itmo.eventapp.main.model.entity.enums.LoginType;
-import org.itmo.eventapp.main.model.validation.validator.PasswordMatchingValidator;
 import org.itmo.eventapp.main.model.validation.validator.ValidLoginValidator;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Constraint(validatedBy = ValidLoginValidator.class)
 @Target({ElementType.TYPE})
