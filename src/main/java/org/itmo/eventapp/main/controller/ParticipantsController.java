774bd8761eb571c21dae6f6282e231394f27778f
daria <dashashahova11@gmail.com>
2024-04-11 13:46:34 +0300

    export of files

--- src/main/java/org/itmo/eventapp/main/controller/ParticipantsController.java
+++ src/main/java/org/itmo/eventapp/main/controller/ParticipantsController.java
@@ -11,6 +11,8 @@ import org.itmo.eventapp.main.model.entity.Participant;
 import org.itmo.eventapp.main.model.mapper.ParticipantMapper;
 import org.itmo.eventapp.main.service.ParticipantsService;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -19,6 +21,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -46,11 +50,20 @@ public class ParticipantsController {
         return ResponseEntity.ok().body(ParticipantMapper.participantsToResponseList(participants));
     }
 
-    @GetMapping("/xlsxExport")
-    @ResponseBody
-    public ResponseEntity<FileSystemResource> getParticipantsXlsxFile(@PathVariable("id") Integer id) throws IOException {
-        File participants = participantsService.getParticipantsXlsx(id);
-        return ResponseEntity.ok().body(new FileSystemResource(participants));
+    @GetMapping("/download")
+    public ResponseEntity<Resource> getParticipantsXlsxFile(@PathVariable("id") Integer id) throws IOException {
+        String path = participantsService.getParticipantsXlsx(id);
+        Path filePath = Paths.get(path);
+        Resource fileResource = new FileSystemResource(filePath);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + filePath.getFileName().toString() + "\"");
+        headers.set("Content-Type","application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(filePath.toFile().length())
+                .body(fileResource);
     }
 
 }
