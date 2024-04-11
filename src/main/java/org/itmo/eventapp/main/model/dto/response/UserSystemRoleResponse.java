c962b887d910d8b096eba0b95a85e763febb319d
Alexander Kuznecov <darklol545@gmail.com>
2024-04-11 21:53:37 +0300

    fix: user list controller forgotten id

--- src/main/java/org/itmo/eventapp/main/model/dto/response/UserSystemRoleResponse.java
+++ src/main/java/org/itmo/eventapp/main/model/dto/response/UserSystemRoleResponse.java
@@ -1,16 +1,19 @@
 package org.itmo.eventapp.main.model.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.itmo.eventapp.main.model.entity.enums.LoginType;
 
 public record UserSystemRoleResponse (
-            String login,
-
-            LoginType type,
-
+            Integer id,
+            @Schema(example = "Иван")
             String name,
-
+            @Schema(example = "Иванов")
             String surname,
-
+            @Schema(example = "ivan@itmo.ru")
+            String login,
+            @Schema(example = "EMAIL")
+            LoginType type,
+            @Schema(example = "Читатель")
             String role
 ) {
 }
