5d08481bbe25d944dca12f90d4311fbb23278cd6
daria <dashashahova11@gmail.com>
2024-04-11 15:44:48 +0300

    add preAuthorize and schema annotations

--- src/main/java/org/itmo/eventapp/main/controller/ParticipantsController.java
+++ src/main/java/org/itmo/eventapp/main/controller/ParticipantsController.java
@@ -1,11 +1,17 @@
 package org.itmo.eventapp.main.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.itmo.eventapp.main.model.dto.request.CreateEventRequest;
 import org.itmo.eventapp.main.model.dto.request.ParticipantPresenceRequest;
 import org.itmo.eventapp.main.model.dto.request.ParticipantsListRequest;
+import org.itmo.eventapp.main.model.dto.response.EventResponse;
 import org.itmo.eventapp.main.model.dto.response.ParticipantResponse;
 import org.itmo.eventapp.main.model.entity.Participant;
 import org.itmo.eventapp.main.model.mapper.ParticipantMapper;
@@ -15,6 +21,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -32,24 +39,60 @@ import java.util.List;
 public class ParticipantsController {
     private final ParticipantsService participantsService;
 
+    @Operation(summary = "Получение списка участников мероприятия")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            schema = @Schema(implementation = ParticipantResponse.class))
+                            })
+            })
+    @PostMapping
     @GetMapping
     public ResponseEntity<List<ParticipantResponse>> getParticipants(@Min(1) @PathVariable("id") Integer id) {
         List<Participant> participants = participantsService.getParticipants(id);
         return ResponseEntity.ok().body(ParticipantMapper.participantsToResponseList(participants));
     }
 
+
+    @Operation(summary = "Изменения поля visited у участника мероприятия")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            schema = @Schema(implementation = ParticipantResponse.class))
+                            })
+            })
+    @PreAuthorize("@participantsSecurityExpression.canWorkWithList(#id)")
     @PutMapping
     public ResponseEntity<ParticipantResponse> changePresence(@PathVariable("id") Integer id, @RequestBody ParticipantPresenceRequest participantPresenceRequest) {
         Participant participant = participantsService.changePresence(id, participantPresenceRequest);
         return ResponseEntity.ok().body(ParticipantMapper.participantToResponse(participant));
     }
 
+    @Operation(summary = "Импорт списка участников мероприятия")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            schema = @Schema(implementation = ParticipantResponse.class))
+                            })
+            })
+    @PreAuthorize("@participantsSecurityExpression.canImportList(#id)")
     @PostMapping
     public ResponseEntity<List<ParticipantResponse>> setPartisipantsList(@PathVariable("id") Integer id, @RequestBody ParticipantsListRequest participantsListRequest) throws IOException {
         List<Participant> participants = participantsService.setParticipants(id, participantsListRequest);
         return ResponseEntity.ok().body(ParticipantMapper.participantsToResponseList(participants));
     }
 
+    @Operation(summary = "Экспорт списка участников мероприятия")
+    @PreAuthorize("@participantsSecurityExpression.canExportList(#id)")
     @GetMapping("/download")
     public ResponseEntity<Resource> getParticipantsXlsxFile(@PathVariable("id") Integer id) throws IOException {
         String path = participantsService.getParticipantsXlsx(id);
---
+++ src/main/java/org/itmo/eventapp/main/security/securityexpression/ParticipantsSecurityExpression.java
@@ -0,0 +1,33 @@
+package org.itmo.eventapp.main.security.securityexpression;
+
+import jakarta.validation.constraints.Min;
+import lombok.RequiredArgsConstructor;
+import org.itmo.eventapp.main.model.entity.enums.PrivilegeName;
+import org.itmo.eventapp.main.service.EventRoleService;
+import org.itmo.eventapp.main.service.EventService;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+@RequiredArgsConstructor
+@Validated
+@Service
+public class ParticipantsSecurityExpression {
+    private final MiscSecurityExpression miscSecurityExpression;
+
+
+    public boolean canImportList(@Min(value = 1, message = "Параметр eventId не может быть меньше 1") int eventId) {
+        eventId = miscSecurityExpression.getParentEventOrSelfId(eventId);
+        return miscSecurityExpression.checkEventPrivilege(eventId, PrivilegeName.IMPORT_PARTICIPANT_LIST_XLSX);
+    }
+
+
+    public boolean canWorkWithList(@Min(value = 1, message = "Параметр eventId не может быть меньше 1") int eventId) {
+        eventId = miscSecurityExpression.getParentEventOrSelfId(eventId);
+        return miscSecurityExpression.checkEventPrivilege(eventId, PrivilegeName.WORK_WITH_PARTICIPANT_LIST);
+    }
+
+    public boolean canExportList(@Min(value = 1, message = "Параметр eventId не может быть меньше 1") int eventId) {
+        eventId = miscSecurityExpression.getParentEventOrSelfId(eventId);
+        return miscSecurityExpression.checkEventPrivilege(eventId, PrivilegeName.EXPORT_PARTICIPANT_LIST_XLSX);
+    }
+}
