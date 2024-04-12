a89e274ab27364320e76c476f9898cb8c878d67a
Roman Glinskikh <glinskikh.roman@yandex.ru>
2024-04-12 23:41:37 +0300

    init

--- src/main/java/org/itmo/eventapp/main/model/dto/response/PrivilegeWithHasOrganizerRolesResponse.java
+++ src/main/java/org/itmo/eventapp/main/model/dto/response/PrivilegeWithHasOrganizerRolesResponse.java
@@ -3,6 +3,8 @@ package org.itmo.eventapp.main.model.dto.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 import org.itmo.eventapp.main.model.entity.enums.PrivilegeName;
 
+import java.util.List;
+
 public record PrivilegeWithHasOrganizerRolesResponse(
         List<PrivilegeResponse> privileges,
 
