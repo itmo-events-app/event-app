f67c11993e98a49ab82d62258a05cb4e1ed46f47
a.verz <alxverzz@gmail.com>
2024-04-13 17:15:39 +0300

    fix: add imports

--- src/main/java/org/itmo/eventapp/main/security/util/JwtTokenUtil.java
+++ src/main/java/org/itmo/eventapp/main/security/util/JwtTokenUtil.java
@@ -5,8 +5,10 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Key;
 import java.time.Instant;
