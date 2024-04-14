9f0f77aac0b0ba06943476454672cb644bbae828
ip13 <iperevozchikov2002@gmail.com>
2024-04-14 19:29:05 +0300

    feaet: message to date validation added

--- src/main/java/org/itmo/eventapp/main/model/dto/request/EventRequest.java
+++ src/main/java/org/itmo/eventapp/main/model/dto/request/EventRequest.java
@@ -4,21 +4,28 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 import org.itmo.eventapp.main.model.entity.enums.EventFormat;
 import org.itmo.eventapp.main.model.entity.enums.EventStatus;
+import org.itmo.eventapp.main.model.validation.annotation.StartDateBeforeEndDate;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
 
-// TODO: Add correct annotations
+import static org.itmo.eventapp.main.exceptionhandling.ExceptionConst.*;
+
+@StartDateBeforeEndDate.List({
+    @StartDateBeforeEndDate(first = "startDate", second = "endDate", message = EVENT_START_TO_END_VALIDATION),
+    @StartDateBeforeEndDate(first = "registrationStart", second = "registrationEnd", message = EVENT_REGISTRATION_START_TO_END_VALIDATION),
+    @StartDateBeforeEndDate(first = "preparingStart", second = "preparingEnd", message = EVENT_PREPARATION_START_TO_END_VALIDATION)
+})
 public record EventRequest(
     @Min(value = 1, message = "Поле eventId не может быть меньше 1!")
     @NotNull(message = "Поле placeId не может быть null!")
     @Schema(example = "67", requiredMode = Schema.RequiredMode.REQUIRED)
     Integer placeId,
-    @FutureOrPresent
+    @FutureOrPresent(message = "Мероприятие не может начинаться в прошлом")
     @NotNull(message = "Поле startDate не может быть null!")
     @Schema(example = "2024-09-01Е12:00:00", requiredMode = Schema.RequiredMode.REQUIRED)
     LocalDateTime startDate,
-    @Future
+    @Future(message = "Мероприятие не может закончится в прошлом")
     @NotNull(message = "Поле endDate не может быть null!")
     @Schema(example = "2024-09-01Е17:00:00", requiredMode = Schema.RequiredMode.REQUIRED)
     LocalDateTime endDate,
@@ -37,11 +44,11 @@ public record EventRequest(
     @NotNull(message = "Поле status не может быть null!")
     @Schema(example = "PUBLISHED", requiredMode = Schema.RequiredMode.REQUIRED)
     EventStatus status,
-    @FutureOrPresent
+    @FutureOrPresent(message = "Регистрация на мероприятие не может начинаться в прошлом")
     @NotNull(message = "Поле registrationStart не может быть null!")
     @Schema(example = "2024-08-01Е12:00:00", requiredMode = Schema.RequiredMode.REQUIRED)
     LocalDateTime registrationStart,
-    @Future
+    @Future(message = "Регистрация на мероприятие не может закончится в прошлом")
     @NotNull(message = "Поле registrationEnd не может быть null!")
     @Schema(example = "2024-08-31Е12:00:00", requiredMode = Schema.RequiredMode.REQUIRED)
     LocalDateTime registrationEnd,
@@ -61,11 +68,11 @@ public record EventRequest(
     @Max(value = 150, message = "Поле participantAgeHighest не может быть больше 150!")
     @Schema(example = "30", requiredMode = Schema.RequiredMode.REQUIRED)
     int participantAgeHighest,
-    @Future
+    @FutureOrPresent(message = "Подготовка к мероприятию не может начинаться в прошлом")
     @NotNull(message = "Поле preparingStart не может быть null!")
     @Schema(example = "2024-08-01Е12:00:00", requiredMode = Schema.RequiredMode.REQUIRED)
     LocalDateTime preparingStart,
-    @Future
+    @Future(message = "Подготовка к мероприятию не может закончится в прошлом")
     @NotNull(message = "Поле preparingEnd не может быть null!")
     @Schema(example = "2024-08-31Е12:00:00", requiredMode = Schema.RequiredMode.REQUIRED)
     LocalDateTime preparingEnd,
--- src/main/java/org/itmo/eventapp/main/model/dto/request/TaskRequest.java
+++ src/main/java/org/itmo/eventapp/main/model/dto/request/TaskRequest.java
@@ -1,11 +1,18 @@
 package org.itmo.eventapp.main.model.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.itmo.eventapp.main.model.entity.enums.TaskStatus;
+import org.itmo.eventapp.main.model.validation.annotation.StartDateBeforeEndDate;
 
 import java.time.LocalDateTime;
 
+import static org.itmo.eventapp.main.exceptionhandling.ExceptionConst.TASK_NOTIFICATION_TO_DEADLINE_VALIDATION;
+
+@StartDateBeforeEndDate(first = "reminder", second = "deadline", message = TASK_NOTIFICATION_TO_DEADLINE_VALIDATION)
 public record TaskRequest(
     @NotNull
     @Min(value = 1, message = "Поле eventId не может быть меньше 1!")
@@ -24,10 +31,10 @@ public record TaskRequest(
     @Min(value = 1, message = "Поле placeId не может быть меньше 1!")
     @Schema(example = "1")
     Integer placeId,
-    @FutureOrPresent
+    @Future(message = "Нельзя назначить крайний срок выполнения задачи в прошлое")
     @NotNull(message = "Поле deadline не может быть null!")
     LocalDateTime deadline,
-    @Future
+    @Future(message = "Нельзя назначить уведомление об окончании задачи в прошлое")
     @NotNull(message = "Поле reminder не может быть null!")
     LocalDateTime reminder
 ) {
